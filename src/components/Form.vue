46c54e9565cdfe8e1e453851786dc81c1edf3a66
Admin <klauss.elephant@gmail.com>
2022-03-09 11:43:09 +0700

    form is done/2

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -20,75 +20,75 @@
               type="text" v-model="username"
               placeholder=" Введите наименование товара"
               class="input">
-                    <p
-                        :class="{error_mess: isUserNameError}"
-                        v-if="isUserNameError">
-                        Поле является обязательным
-                    </p>
+          <p
+              :class="{error_mess: isUserNameError}"
+              v-if="isUserNameError">
+            Поле является обязательным
+          </p>
         </div>
 
-                <div class="prod-form__block">
+        <div class="prod-form__block">
 
-                  <div class="prod-form__label-block">
-                    <label class="label">Описание товара</label>
-                    <div class="circle"></div>
-                  </div>
+          <div class="prod-form__label-block">
+            <label class="label">Описание товара</label>
+            <div class="circle"></div>
+          </div>
 
-                  <textarea
-                      :class="{error_input: isTextareaError}"
-                      class=textarea
-                      v-model="textarea"
-                      placeholder=" Введите описание товара"></textarea>
-                  <p
-                      :class="{error_mess: isTextareaError}"
-                      v-if="isTextareaError">
-                    Поле является обязательным
-                  </p>
-                </div>
+          <textarea
+              :class="{error_input: isTextareaError}"
+              class=textarea
+              v-model="textarea"
+              placeholder=" Введите описание товара"></textarea>
+          <p
+              :class="{error_mess: isTextareaError}"
+              v-if="isTextareaError">
+            Поле является обязательным
+          </p>
+        </div>
 
-                <div class="prod-form__block">
+        <div class="prod-form__block">
 
-                  <div class="prod-form__label-block">
-                    <label class="label">Ссылка на изображение товара</label>
-                    <div class="circle"></div>
-                  </div>
+          <div class="prod-form__label-block">
+            <label class="label">Ссылка на изображение товара</label>
+            <div class="circle"></div>
+          </div>
 
-                  <input
-                      :class="{error_input: isUrlError}"
-                      type="url"
-                      v-model="url"
-                      placeholder="Введите ссылку"
-                      class="input">
-                  <p
-                      :class="{error_mess: isUrlError}"
-                      v-if="isUrlError">
-                      Поле является обязательным
-                  </p>
-                </div>
+          <input
+              :class="{error_input: isUrlError}"
+              type="url"
+              v-model="url"
+              placeholder="Введите ссылку"
+              class="input">
+          <p
+              :class="{error_mess: isUrlError}"
+              v-if="isUrlError">
+            Поле является обязательным
+          </p>
+        </div>
 
-                <div class="prod-form__block">
+        <div class="prod-form__block">
 
-                  <div class="prod-form__label-block">
-                    <label class="label">Цена товара</label>
-                    <div class="circle"></div>
-                  </div>
+          <div class="prod-form__label-block">
+            <label class="label">Цена товара</label>
+            <div class="circle"></div>
+          </div>
 
-                  <input
-                      :class="{error_input: isNumberError}"
-                      :type="indicatorChange ? 'number' : 'text'"
-                      v-model="modelNumber"
-                      step="1" min="1" max="100000000"
-                      placeholder=" Введите цену"
-                      @focus="indicatorChange = true"
-                      @blur="indicatorChange = false"
-                      class="input"
-                  >
-                  <p
-                      :class="{error_mess: isNumberError}"
-                      v-if="isNumberError">
-                    Поле является обязательным
-                  </p>
-                </div>
+          <input
+              :class="{error_input: isNumberError}"
+              :type="indicatorChange ? 'number' : 'text'"
+              v-model="modelNumber"
+              step="1" min="1" max="100000000"
+              placeholder=" Введите цену"
+              @focus="indicatorChange = true"
+              @blur="indicatorChange = false"
+              class="input"
+          >
+          <p
+              :class="{error_mess: isNumberError}"
+              v-if="isNumberError">
+            Поле является обязательным
+          </p>
+        </div>
 
         <button
             :disabled="!submitForm"
@@ -127,13 +127,6 @@ export default {
       }
 
     },
-    // submitForm() {
-    //   if (this.checkForm) {
-    //     return;
-    //   }
-    //   alert("Форма отправлена");
-    // },
-
   },
   computed: {
     isUserNameError() {
